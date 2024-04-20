cebdef3edd479b8fddad4e3300f13dadf509303a
Eli Young <eliyoung4now@gmail.com>
2024-04-20 13:40:38 -0400

    wrote some of main console

--- MasterController.py
+++ MasterController.py
@@ -3,16 +3,47 @@ import sys
 
 
 def main():
+    isString = True
+    while isString:
+        name = input("Patients name?")
+        if type(name) == str:
+            isString = False
+        else:
+            print("Please enter your name.")
 
-    name = input("Patients name?")
-    age = input("Patients age?")
-    gender = input("Patients gender?")
-    description = input("Description of appointment:")
-    familiarity = input("Are they are new patient? Y/N")
+    isAge = True
+    while isAge:
+        age = input("Patients age?")
+        if type(age) == int:
+            isAge = False
+        else:
+            print("Please enter your age.")
 
+    isGender = True
+    while isGender:
+        gender = input("Patients gender?")
+        if type(gender) == str:
+            isGender = False
+        else:
+            print("Please enter your gender.")
+    
+    isDescription = True:
+    while isDescription:
+        description = input("Description of appointment:")
+        if type(description) == str:
+            isDescription = False
+        else:
+            print("Please enter your description.")
 
-    user = UserInterface(name,age,gender,description)
+    isYN = True
+    while isYN:
+        familiarity = input("Are they are new patient? Y/N")
+        if familiarity == "y" or familiarity == "n" or familiarity == "Y" or familiarity == "N":
+            isDescription = False
+        else:
+            print("Please enter your description.")
 
+    user = UserInterface(name, age, gender, description, familiarity)
 
 
 
