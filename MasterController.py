5fe03cb861f239fa482549e178a015cedd2b838a
Eli Young <eliyoung4now@gmail.com>
2024-04-20 14:52:16 -0400

    space

--- MasterController.py
+++ MasterController.py
@@ -5,48 +5,48 @@ import sys
 def main():
     isString = True
     while isString:
-        name = input("Patients name?")
+        name = input("Patients name? ")
         if type(name) == str:
             isString = False
         else:
             print("Please enter your name.")
-            name = input("Patients name?")
+            name = input("Patients name? ")
 
     isAge = True
     while isAge:
-        age = input("Patients age?")
+        age = input("Patients age? ")
         if type(age) == int:
             isAge = False
         else:
             print("Please enter your age.")
-            age = input("Patients age?")
+            age = input("Patients age? ")
 
     isGender = True
     while isGender:
-        gender = input("Patients gender?")
+        gender = input("Patients gender? ")
         if type(gender) == str:
             isGender = False
         else:
             print("Please enter your gender.")
-            gender = input("Patients gender?")
+            gender = input("Patients gender? ")
     
     isDescription = True
     while isDescription:
-        description = input("Description of appointment:")
+        description = input("Description of appointment: ")
         if type(description) == str:
             isDescription = False
         else:
             print("Please enter your description.")
-            description = input("Description of appointment:")
+            description = input("Description of appointment: ")
 
     isYN = True
     while isYN:
-        familiarity = input("Are they are new patient? Y/N")
+        familiarity = input("Are they are new patient? Y/N ")
         if familiarity == "y" or familiarity == "n" or familiarity == "Y" or familiarity == "N":
             isDescription = False
         else:
             print("Please enter your description.")
-            familiarity = input("Are they are new patient? Y/N")
+            familiarity = input("Are they are new patient? Y/N ")
 
     user = UserInterface(name, age, gender, description, familiarity)
 
