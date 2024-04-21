3ef28fe697e2e85a01c853ea3a8b460ff6195e9a
Eli Young <eliyoung4now@gmail.com>
2024-04-20 20:52:16 -0400

    pain

--- nodejs-docker-web-app/server.js
+++ nodejs-docker-web-app/server.js
@@ -1,31 +1,3 @@
-// import express from "express";
-// import dotenv from "dotenv";
-// dotenv.config();
-
-// const PORT = process.env.PORT || 8080;
-// const app = express();
-
-// app.get("/", (req, res) => {
-//     res.json({ message: " We have mounted the volume to running container" });
-// });
-
-// // Serve static files from the 'frontend' directory
-
-
-// // app.use(express.static(path.join(__dirname, 'frontend')));
-
-// // app.get("/", (req, res) => {
-// //     res.sendFile(path.join(__dirname, 'frontend', 'index.html')); // Send the HTML file
-// // });
-
-
-
-// app.listen(PORT, () => {
-//     console.log(`App running on ${PORT}`);
-// });
-
-
-
 import express from "express";
 import dotenv from "dotenv";
 import path from "path"; // Import the 'path' module
@@ -45,3 +17,26 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
     console.log(`App running on ${PORT}`);
 });
+
+//Below attempted with cpt works w/ msg
+
+// import express from "express";
+// import dotenv from "dotenv";
+// import path from "path"; // Import the 'path' module
+
+// dotenv.config();
+
+// const PORT = process.env.PORT || 8080;
+// const app = express();
+
+// // Serve static files from the 'frontend' directory
+// app.use(express.static(path.join(__dirname, 'frontend')));
+
+// // Serve the UI (HTML page) for the root route
+// app.get("/", (req, res) => {
+//     res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
+// });
+
+// app.listen(PORT, () => {
+//     console.log(`App running on ${PORT}`);
+// });
