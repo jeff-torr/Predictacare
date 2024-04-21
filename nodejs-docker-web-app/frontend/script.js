dab11b11a6351359b316432de93daa30df12db09
Eli Young <eliyoung4now@gmail.com>
2024-04-21 00:38:51 -0400

    finished getting data from frontend, need to link to backend and back

--- nodejs-docker-web-app/frontend/script.js
+++ nodejs-docker-web-app/frontend/script.js
@@ -5,3 +5,25 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 //depending on age, gender, etc. multiply the minute value that comes back from bakcend by a scalar before returning it to user
+
+function getClassData() {
+    var nameField = document.getElementById('nameField').value;
+    var ageField = document.gerElementByID('ageField').value;
+    var genderField = document.gerElementByID('genderField').value;
+    var desriptionField = document.gerElementByID('descriptionField').value;
+    var familiarityField = document.gerElementByID('familiarityField').value;
+}
+
+var subButton = document.getElementById('subButton');
+subButton.addEventListener('click', getClassData, false); 
+// submits form (calls above function) when button clicked
+
+class UserData100 {
+    constructor(name, age, gender, description, familiarity) {
+      this.name = nameField;
+      this.age = ageField;
+      this.gender = genderFIeld;
+      this.description = descriptionField;
+      this.familiarity = familiarityField;
+    }
+  }
