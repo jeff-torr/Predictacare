92595baa3c792b485f07eb5e50f716c7699ece63
Eli Young <eliyoung4now@gmail.com>
2024-04-20 20:12:32 -0400

    docker troubleshooting

--- nodejs-docker-web-app/server.js
+++ nodejs-docker-web-app/server.js
@@ -9,6 +9,13 @@ app.get("/", (req, res) => {
     res.json({ message: " We have mounted the volume to running container" });
 });
 
+// Serve static files from the 'frontend' directory
+// app.use(express.static(path.join(__dirname, 'frontend')));
+
+// app.get("/", (req, res) => {
+//     res.sendFile(path.join(__dirname, 'frontend', 'index.html')); // Send the HTML file
+// });
+
 app.listen(PORT, () => {
     console.log(`App running on ${PORT}`);
 });
