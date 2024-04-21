5ca6dbf0c3631d8534c471223231c4e0b7d56cf8
Eli Young <eliyoung4now@gmail.com>
2024-04-20 21:53:11 -0400

    notes

--- nodejs-docker-web-app/frontend/script.js
+++ nodejs-docker-web-app/frontend/script.js
@@ -1,3 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log('Script loaded successfully!');
 });
+
+
+
+//depending on age, gender, etc. multiply the minute value that comes back from bakcend by a scalar before returning it to user
