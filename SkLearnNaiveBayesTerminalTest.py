b24328d42bae0e77f42916e6251c961a77c34638
EYoung21 <eliyoung4now@email.com>
2024-06-07 02:14:03 -0400

    made some progress on linear regression model

--- SkLearnNaiveBayesTerminalTest.py
+++ SkLearnNaiveBayesTerminalTest.py
@@ -2,7 +2,7 @@ import pandas as pd
 import pickle
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
-#from sklearn.naive_bayes import MultinomialNB
+from sklearn.naive_bayes import MultinomialNB
 from sklearn.linear_model import LinearRegression
 import nltk
 from nltk.corpus import stopwords
@@ -51,11 +51,12 @@ class DataHandler:
         return processed_sentences
 
 
-class NaiveBayesModel:
+class LinearRegressionModel:
     def __init__(self):
         #instantiate model Class objs
         self.vectorizer = TfidfVectorizer(ngram_range=(1, 2))
-        self.model = LinearRegression(alpha=0.1)
+        # self.model = LinearRegression(alpha=0.1)
+        self.model = LinearRegression()
 
     def train(self, X_train, y_train):
         # X is matrix of samples to features
@@ -65,8 +66,8 @@ class NaiveBayesModel:
         # fit model with Tfidvectors
         self.model.fit(X_train_counts, y_train)
 
-    def predict(self, descriptions):
-        counts = self.vectorizer.transform(descriptions)
+    def predict(self, descriptions, age, gender):
+        counts = self.vectorizer.transform(descriptions, age, gender)
         return self.model.predict(counts)
 
     def evaluate(self, X_test, y_test):
@@ -85,32 +86,175 @@ def main():
 
     # load and preprocess data
     data_handler = DataHandler(data_file)
-    gender, age, sentences, duration = data_handler.load_data()
+    gender, age, sentences, durations = data_handler.load_data()
     processed_sentences = data_handler.preprocess_data(sentences)
 
     #rearange so splitting rest of variables
-    #have a column for gender age age
+    #have a column for gender age
     #gender convert into 0s and 1s
-    #turn sentecnes into matrix where each row is training ex and row is count of number of words in doc
+    #turn sentecnes into matrix where each row is training ex and column is count of number of words in doc
     #append to matrix columns for gender and age
+    
+    for i in range(len(gender)):
+        if gender[i] == "Female":
+            gender[i] = 0
+        elif gender[i] == "Male":
+            gender[i] = 1
+    
+
 
     # split the dataset
-    X_train, X_test, y_train, y_test = train_test_split(processed_sentences, durations, test_size=0.2, random_state=50)
+    X_train, X_test, y_train, y_test = train_test_split(gender, age, processed_sentences, durations, test_size=0.2, random_state=50)
 
     # train Naive Bayes Model
-    nb_model = NaiveBayesModel()
-    nb_model.train(X_train, y_train)
-    nb_model.evaluate(X_test, y_test)
-    nb_model.save()
+    lr_model = LinearRegressionModel()
+    lr_model.train(X_train, y_train)
+    lr_model.evaluate(X_test, y_test)
+    lr_model.save()
 
     while True:
         user_input = input("Enter an appointment description (or type 'exit' to quit): ")
         if user_input.lower() == 'exit':
             break
+        gender_input = input("Enter gender (0 for Female, 1 for Male): ")
+        age_input = input("Enter age: ")
         processed_input = data_handler.preprocess_data([user_input])
-        predicted_duration = nb_model.predict(processed_input)
+        predicted_duration = lr_model.predict(processed_input, age_input, gender_input)
         print(f"Predicted Duration: {predicted_duration[0]} minutes")
 
 
 if __name__ == "__main__":
     main()
+
+
+
+
+
+
+
+
+
+
+
+#below = other model code?
+
+
+
+# import pandas as pd
+# import pickle
+# from sklearn.model_selection import train_test_split
+# from sklearn.feature_extraction.text import TfidfVectorizer
+# from sklearn.linear_model import LinearRegression
+# import nltk
+# from nltk.corpus import stopwords
+# from nltk.stem import WordNetLemmatizer
+# from scipy.sparse import hstack
+# import numpy as np
+
+# nltk.download('punkt')
+# nltk.download('wordnet')
+# nltk.download('stopwords')
+
+
+# class DataHandler:
+#     def __init__(self, filename):
+#         self.filename = filename
+#         self.ensure_nltk_packages()
+#         self.lemmatizer = WordNetLemmatizer()
+#         self.stop_words = set(stopwords.words('english'))
+
+#     def ensure_nltk_packages(self):
+#         nltk_packages = ['punkt', 'wordnet', 'stopwords']
+#         for package in nltk_packages:
+#             try:
+#                 nltk.data.find(f'tokenizers/punkt/{package}.pickle')
+#             except LookupError:
+#                 nltk.download(package)
+
+#     def load_data(self):
+#         data = pd.read_csv(self.filename)
+#         gender = data.iloc[:, 0].values
+#         age = data.iloc[:, 1].values.astype(int)
+#         sentences = data.iloc[:, 2].values
+#         durations = data.iloc[:, 3].values.astype(int)
+#         return gender, age, sentences, durations
+
+#     def preprocess_data(self, sentences):
+#         processed_sentences = []
+#         for sentence in sentences:
+#             words = nltk.word_tokenize(sentence)
+#             lemmatized = [self.lemmatizer.lemmatize(word.lower())
+#                           for word in words if word.lower() not in self.stop_words]
+#             processed_sentences.append(' '.join(lemmatized))
+#         return processed_sentences
+
+
+# class LinearRegressionModel:
+#     def __init__(self):
+#         self.vectorizer = TfidfVectorizer(ngram_range=(1, 2))
+#         self.model = LinearRegression()
+
+#     def train(self, X_train_text, X_train_features, y_train):
+#         X_train_counts = self.vectorizer.fit_transform(X_train_text)
+#         X_train_combined = hstack([X_train_counts, X_train_features])
+#         self.model.fit(X_train_combined, y_train)
+
+#     def predict(self, descriptions, additional_features):
+#         counts = self.vectorizer.transform(descriptions)
+#         X_input = hstack([counts, additional_features])
+#         predictions = self.model.predict(X_input)
+#         return predictions
+
+#     def evaluate(self, X_test_text, X_test_features, y_test):
+#         X_test_counts = self.vectorizer.transform(X_test_text)
+#         X_test_combined = hstack([X_test_counts, X_test_features])
+#         predictions = self.model.predict(X_test_combined)
+#         mse = np.mean((predictions - y_test) ** 2)
+#         print(f"Mean Squared Error: {mse}")
+
+#     def save(self, path='linear_regression_model.pickle'):
+#         with open(path, 'wb') as handle:
+#             pickle.dump((self.vectorizer, self.model), handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+# def main():
+#     data_file = 'EstimateHealthcareAppointmentLengthGivenX-Sheet1.csv'
+
+#     # Load and preprocess data
+#     data_handler = DataHandler(data_file)
+#     gender, age, sentences, durations = data_handler.load_data()
+#     processed_sentences = data_handler.preprocess_data(sentences)
+
+#     # Encode gender to numeric
+#     gender_encoded = np.where(gender == "Female", 0, 1)
+
+#     # Combine features into a single matrix
+#     additional_features = pd.DataFrame({
+#         'gender': gender_encoded,
+#         'age': age
+#     })
+
+#     # Split the dataset
+#     X_train_text, X_test_text, X_train_features, X_test_features, y_train, y_test = train_test_split(
+#         processed_sentences, additional_features.values, durations, test_size=0.2, random_state=50)
+
+#     # Train and evaluate the model
+#     model = LinearRegressionModel()
+#     model.train(X_train_text, X_train_features, y_train)
+#     model.evaluate(X_test_text, X_test_features, y_test)
+#     model.save()
+
+#     while True:
+#         user_input = input("Enter an appointment description (or type 'exit' to quit): ")
+#         if user_input.lower() == 'exit':
+#             break
+#         gender_input = int(input("Enter gender (0 for Female, 1 for Male): "))
+#         age_input = int(input("Enter age: "))
+#         processed_input = data_handler.preprocess_data([user_input])
+#         additional_features_input = pd.DataFrame({'gender': [gender_input], 'age': [age_input]})
+#         predicted_duration = model.predict(processed_input, additional_features_input.values)
+#         print(f"Predicted Duration: {predicted_duration[0]} minutes")
+
+
+# if __name__ == "__main__":
+#     main()
